96f641702b1d4ededafece451996b1c5bce2c8f1
Mikhail <mb.38@yandex.ru>
2020-03-07 12:09:58 +0700

    first commit

--- src/common/js/slider.js
+++ src/common/js/slider.js
@@ -3,15 +3,15 @@ let swiper = new Swiper(document.querySelectorAll('.swiper-container'), {
     direction: 'horizontal',
     speed: 800,
     keyboard: {
-      enabled: true,
-      onlyInViewport: true,
+        enabled: true,
+        onlyInViewport: true,
     },
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
     },
     navigation: {
-      nextEl: '.slider-navigation__next',
-      prevEl: '.slider-navigation__prev',
+        nextEl: '.slider-navigation__next',
+        prevEl: '.slider-navigation__prev',
     },
   });
